1ef56a512462b0b9ac3cefca406095f190d8af3b
Kavindu Jayarathne <kavindujayarathne.dev@gmail.com>
2025-09-30 23:03:47 +0530

    Initial commit

---
+++ Formula/macpak.rb
@@ -0,0 +1,43 @@
+class Macpak < Formula
+  desc "Interactive Homebrew helper + leftover zapper for non-brew apps"
+  homepage "https://github.com/kavindujayarathne/macpak"
+  url "https://github.com/kavindujayarathne/macpak/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "479ffb0e079656ed6287e3ba42d975e5edd8e98465b598ac4e41d147c8eebb25"
+  license "MIT"
+  head "https://github.com/kavindujayarathne/macpak.git", branch: "main"
+
+  depends_on "fzf"
+
+  def install
+    libexec.install "lib", "bin/macpak"
+    (bin/"macpak").write_env_script libexec/"macpak", {
+      :MACPAK_VERSION => version
+    }
+
+    bash_completion.install "completions/macpak"
+    zsh_completion.install  "completions/_macpak"
+  end
+
+  def caveats
+    <<~EOS
+      zsh completions load automatically if your shell runs `compinit`.
+
+      Bash users:
+        - If you have the 'bash-completion' package installed and sourced,
+          the completion file in #{HOMEBREW_PREFIX}/etc/bash_completion.d/macpak
+          will be picked up automatically.
+        - Otherwise, source it manually in your ~/.bashrc:
+            [[ -r "#{HOMEBREW_PREFIX}/etc/bash_completion.d/macpak" ]] && \
+              . "#{HOMEBREW_PREFIX}/etc/bash_completion.d/macpak"
+
+      Optional tools you may want:
+        - trash (used when USE_TRASH=1; macOS 14+ has /usr/bin/trash)
+        - tmux  (only for the pager split in previews)
+    EOS
+  end
+
+  test do
+    assert_match "macpak", shell_output("#{bin}/macpak -v")
+    system "#{bin}/macpak", "--help"
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Tap: macpak
+
+This is the official [Homebrew tap](https://docs.brew.sh/Taps) for [macpak](https://github.com/kavindujayarathne/macpak).
+
+## Installation
+
+```bash
+brew tap kavindujayarathne/macpak
+brew install --formula macpak
+```
+
+## Install from HEAD
+
+```bash
+brew install --HEAD macpak
+```
